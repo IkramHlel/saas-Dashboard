293862b0a745ebbcd3a1471091e715746df08eb8
Ikram <ikram.hlel8@gmail.com>
2026-04-12 16:37:14 +0200

    fix: remove unused metaValue prop in metric card

--- src/components/UI/MetricCard.tsx
+++ src/components/UI/MetricCard.tsx
@@ -9,7 +9,7 @@ interface MetricCardProps {
   metaValue?: string;
 }
 
-const MetricCard = ({ titles, values, icon, metaValue = '+0.00%' }: MetricCardProps) => {
+const MetricCard = ({ titles, values, icon }: MetricCardProps) => {
     const options = [
   { label: "This week", value: "week" },
   { label: "This month", value: "month" },
